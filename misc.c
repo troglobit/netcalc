7a4f2acbc664678fb1f24a63dae4f0508a656fa4
Joachim Nilsson <troglobit@gmail.com>
2015-10-27 23:46:33 +0100

    Add missing include files.
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- misc.c
+++ misc.c
@@ -28,7 +28,9 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
+#include <ctype.h>		/* isxdigit() */
 #include <stdio.h>
+#include <stdlib.h>		/* atoi(), strtoul(), strtol() */
 #include <string.h>
 
 #include "netcalc.h"
