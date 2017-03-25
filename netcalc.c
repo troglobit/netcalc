5753cb9ce25bcec146a86bad66b4c9ab1333c5a8
Joachim Nilsson <troglobit@gmail.com>
2017-03-25 20:09:01 +0100

    Relocate usage() function
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- netcalc.c
+++ netcalc.c
@@ -438,6 +438,31 @@ struct if_info *parse_abox(struct argbox *abox, struct if_info *if_start)
 	return ifarg_start;
 }
 
+static int usage(int code)
+{
+	printf("Usage: %s [OPTIONS] <NETWORK/LEN | - | NETWORK NETMASK>\n"
+	       "\n"
+	       "Global options:\n"
+	       "  -h       This help text\n"
+	       "  -v       Show version information\n"
+	       "\n"
+	       "IPv4 options:\n"
+	       "  -s MASK  Split the IPv4 network into subnets of MASK size\n"
+	       "\n"
+	       "IPv6 options:\n"
+	       "  -e       IPv4 compatible IPv6 information\n"
+	       "  -r       IPv6 reverse DNS output\n"
+	       "  -S MASK  Split the IPv6 network into subnets of MASK size\n"
+	       "\n"
+	       "Copyright (C) 2003-2013  Simon Ekstrand\n"
+	       "Copyright (C) 2010-2017  Joachim Nilsson\n"
+	       "\n"
+	       "This is free software, under the 3-clause BSD license: you are free to change\n"
+	       "and redistribute it.  There is NO WARRANTY, to the extent permitted by law\n", ident);
+
+	return code;
+}
+
 static char *progname(char *arg0)
 {
        char *nm;
--- output.h
+++ output.h
@@ -18,7 +18,6 @@ void print_v6(struct if_info *ifi);
 void print_v4inv6(struct if_info *ifi);
 int v6plus(struct sip_in6_addr *a, struct sip_in6_addr *b);
 void show_split_networks_v6(struct if_info *ifi, struct sip_in6_addr splitmask, int v6args, struct misc_args m_argv6);
-int usage(int code);
 
 #endif /* NETCALC_OUTPUT_H_ */
 
