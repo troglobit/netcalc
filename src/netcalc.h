af9b2103036162af65e96fd9d1c35e9141c92b47
Joachim Nilsson <troglobit@gmail.com>
2018-09-16 10:29:35 +0200

    Fix #20: Fix build issue on macOS
    Add missing #ifdefs around forward declarations of strlcpy() and
    strlcat().  Some systems actually have these function natively.
    
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/netcalc.h
+++ src/netcalc.h
@@ -229,8 +229,12 @@ int mk_ipv6addr(struct v6addr *in6_addr, char *addr);
 /*
  * strlcpy.c strlcat.c
  */
+#ifndef HAVE_STRLCPY
 size_t strlcpy(char *dst, const char *src, size_t siz);
+#endif
+#ifndef HAVE_STRLCAT
 size_t strlcat(char *dst, const char *src, size_t siz);
+#endif
 
 /*
  * cprintf.c
